99c5e4e4a142a7159b2731623357f72745ca41ec
Leonel Ricardo Carrizo Marin <lcarrizo@c3r5s1.42london.com>
2023-03-04 19:28:02 +0000

    functional version but +25 lines, others

--- main.c
+++ main.c
@@ -4,6 +4,6 @@ int	rush(int x, int y);
 
 int	main(void)
 {
-	rush(5, 3);
+	rush(4, 4);
 	return (0);
 }
--- rush01.c
+++ rush01.c
@@ -1,62 +1,77 @@
 #include <unistd.h>
 
 void	ft_putchar(char c);
-int	i, j, k, m, l;
 
 int	rush(int x, int y)
 {
-
-
-	i = 2;
-	while(i < y)
+	int	i, j, k, m, l;
+	if (x <= 1 && y <= 1)
 	{
-	
-	ft_putchar('/');
+		ft_putchar('/');
+	}
 
-		//first colum
+	else if (x > 1 && y <= 1)
+	{
+		ft_putchar('/');
+		i = 2;
+		while (i < x)
+		{
+		ft_putchar('*');
+		i++;
+		}
+		ft_putchar(92);
+	}
+
+	else if (x <=1 && y > 1)
+	{
+		ft_putchar('/');
+		ft_putchar(10);
 		j = 2;
-		while(j < x)
+		while (j < y)
+		{
+		ft_putchar('*');
+		ft_putchar(10);
+		j++;
+		}
+		ft_putchar(92);
+	}
+
+	else
+	{
+		ft_putchar('/');
+		l = 2;
+		while (l < x)
+		{
+		ft_putchar('*');
+		l++;
+		}
+		ft_putchar(92);
+		ft_putchar(10);
+
+		k = 2;
+		while (k < y)
 		{
 			ft_putchar('*');
-			j++;
+			l = 2;
+			while (l < x)
+			{
+				ft_putchar(32);
+				l++;
+			}
+			ft_putchar('*');
+			ft_putchar(10);
+			k++;
 		}
-	
 
-	ft_putchar(92);
-	ft_putchar(10);
-		//central
-		k = 2;
-		while(k < y)
+		ft_putchar(92);
+		m = 2;
+		while (m < x)
 		{
-		
-		ft_putchar('*');
-		k++;
+			ft_putchar('*');
+			m++;
 		}
-
-		//final
-	ft_putchar(10);
-	ft_putchar(92);
-		m = 0;
-		while(m < y)
-		{
-		ft_putchar('*'); 	
-		m++;
-
-		}
-
-		// l = 2;
-		// while(l < y)
-		// {
-		// ft_putchar('*');
-		// l++;
-		// }
-	ft_putchar('/');
-	
-	i++;
+		ft_putchar('/');
 	}
-	
-
-
 
 	return (0);
 }
